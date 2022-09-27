d8a479cda3932d5e44155e7dfba6726cf43c5a1f
Diana Ramos/ Dinorekkos <diana_ramos_g@hotmail.com>
2022-09-27 16:53:35 -0500

    active ragdoll

---
+++ Dino_AR_SimuPhysics/Assets/Dino/Models/ActiveRoll.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveRoll : MonoBehaviour
+{
+    public GameObject ragdoll;
+    public GameObject reference;
+
+    private List<ConfigurableJoint> _configurableJoints;
+    private List<Transform> _referenceJoints;
+    void Start()
+    {
+        foreach (var VARIABLE in _configurableJoints)
+        {
+            
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
